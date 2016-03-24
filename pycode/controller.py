ed409817be20c19e40e20609d3bf25d102104a72
David Nemirovsky <nemirovsky.dm@gmail.com>
2016-03-24 09:22:41 -0400

    Added Graph and cycle creation to automated param learning portion

--- pycode/controller.py
+++ pycode/controller.py
@@ -46,13 +46,25 @@ class Controller(object):
 
 	def getDetectionsAutomatically(self, image):
 		from TEMAnalysis.OptiAtomDetector import OptiAtomDetector
+		from TEMAnalysis.GraphCycleCreator import Graph
 		self.image = image
 		ad = OptiAtomDetector()
+		g = Graph()
+
+		points = ad.detect(image)
+		cycles, graph = g.findNeighbourhood(image,
+		 									points,
+											ad.segger.ksize,
+											ad.segger.sigma)
+
+
 		return {
 			'type' 		: "pointsets",
 			'pointsets'	: [{
 				'name'	: "Automatic Initial Detections",
-				'points': ad.detect(image).tolist()
+				'points': points.tolist(),
+				'graph' : graph.edges(),
+				'cycles': [cycle.tolist() for cycle in cycles]
 			}]
 		}
 
